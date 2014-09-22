d79ff124d52d9e32177c5ad7709cef658510ef0b
ilovesardine <krisnandikaaji@yahoo.co.id>
2014-09-22 19:51:27 +0700

    Gak penting

--- views/a_header.php
+++ views/a_header.php
@@ -69,7 +69,7 @@
                     <img src="assets/img/avatar5.png" class="img-circle" alt="User Image" />
                 </div>
                 <div class="pull-left info">
-                    <p>Hello, Jane</p>
+                    <p>Hello, admin</p>
 
                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
